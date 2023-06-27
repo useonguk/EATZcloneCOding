fea62136e0c1ef575878a024053ab9951f281aa9
useonguk <ysu0219@gmail.com>
2023-06-27 09:14:53 +0900

    change:header

--- src/components/Headers/index.jsx
+++ src/components/Headers/index.jsx
@@ -9,10 +9,7 @@ function header(){
             <input type='button' value={"검색"}></input>
             <Link to="/login">로그인</Link>
             <nav>
-                <Link to="/"><div>추천</div></Link>
-                <Link to="/character"><div>캐릭터</div></Link>
-                <Link to="/category"><div>카테고리</div></Link>
-                <Link to="/favorite "><div>즐겨찾기</div></Link>
+                <Link to="/"><div>홈으로</div></Link>
             </nav>
         </>
     );
